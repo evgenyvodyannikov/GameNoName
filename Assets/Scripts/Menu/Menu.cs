ea0aa61531cad2b2a192fab7957d4a766cc952e0
123 <vodyannikov.eugene@gmail.com>
2021-09-24 18:14:04 +0500

    a few stngs c# scripts

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Работа с интерфейсами
+using UnityEngine.SceneManagement; //Работа со сценами
+using UnityEngine.Audio; //Работа с аудио
+public class Menu : MonoBehaviour
+{
+
+    public bool isOpened = false; //Открыто ли меню
+    public float volume = 0; //Громкость
+    public int quality = 0; //Качество
+    public bool isFullscreen = false; //Полноэкранный режим
+    public AudioMixer audioMixer; //Регулятор громкости
+    public Dropdown resolutionDropdown; //Список с разрешениями для игры
+    private Resolution[] resolutions; //Список доступных разрешений
+    private int currResolutionIndex = 0; //Текущее разрешение
+
+    public void ShowHideMenu()
+    {
+        isOpened = !isOpened;
+        GetComponent<Canvas>().enabled = isOpened; //Включение или отключение Canvas. Ещё тут можно использовать метод SetActive()
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            ShowHideMenu();
+        }
+    }
+
+    public void ChangeVolume(float val) //Изменение звука
+    {
+        volume = val;
+    }
+
+    public void ChangeResolution(int index) //Изменение разрешения
+    {
+        currResolutionIndex = index;
+    }
+
+    public void ChangeFullscreenMode(bool val) //Включение или отключение полноэкранного режима
+    {
+        isFullscreen = val;
+    }
+
+    public void ChangeQuality(int index) //Изменение качества
+    {
+        quality = index;
+    }
+
+    public void GoToSettings()
+    {
+        Application.Quit();
+    }
+
+    public void SaveSettings()
+    {
+        audioMixer.SetFloat("MasterVolume", volume); //Изменение уровня громкости
+        QualitySettings.SetQualityLevel(quality); //Изменение качества
+        Screen.fullScreen = isFullscreen; //Включение или отключение полноэкранного режима
+        Screen.SetResolution(Screen.resolutions[currResolutionIndex].width, Screen.resolutions[currResolutionIndex].height, isFullscreen); //Изменения разрешения
+    }
+
+}
---
+++ Assets/scr.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("rnjjj");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("rnjjj");
+        Debug.Log("rnjjj");
+    }
+
+
+    public void setonme() {
+    }
+}
